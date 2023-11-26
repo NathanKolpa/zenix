90fdb50912a9553daeb935ef69dd9751b1324cf1
Nathan Kolpa <nathan@kolpa.me>
2023-11-26 03:51:21 +0100

    Kernel: Remove eternal alloc in favor of a simple statically allocated byte array

--- kernel/src/memory.rs
+++ kernel/src/memory.rs
@@ -1,5 +1,5 @@
 mod frame_alloc;
 mod info;
-
+mod kernel_alloc;
 pub use frame_alloc::FRAME_ALLOC;
 pub use info::*;
--- kernel/src/memory/frame_alloc.rs
+++ kernel/src/memory/frame_alloc.rs
@@ -1,8 +1,6 @@
 use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
-use core::sync::atomic::{AtomicUsize, Ordering};
 
 use crate::memory::MemoryInfo;
-use crate::util::address::PhysicalAddress;
 use crate::util::{FixedVec, PanicOnce};
 
 pub struct AllocatedFrame {
@@ -32,7 +30,6 @@ impl FreeList {
 pub struct FrameAllocator<'a> {
     memory_map: PanicOnce<&'a MemoryMap>,
     slots: FixedVec<30, FreeList>,
-    start: AtomicUsize,
 }
 
 impl<'a> FrameAllocator<'a> {
@@ -40,7 +37,6 @@ impl<'a> FrameAllocator<'a> {
         Self {
             memory_map: PanicOnce::new(),
             slots: FixedVec::new(),
-            start: AtomicUsize::new(0),
         }
     }
 
@@ -49,7 +45,7 @@ impl<'a> FrameAllocator<'a> {
     }
 
     pub fn info(&self) -> MemoryInfo {
-        let bytes_allocated = self.start.load(Ordering::Relaxed);
+        let bytes_allocated = 0;
 
         let mut total_allocatable_bytes = 0;
         let mut total_bytes = 0;
@@ -104,85 +100,6 @@ impl<'a> FrameAllocator<'a> {
 
         result
     }
-
-    /// Allocate a chunk of physical memory without the ability to deallocate.
-    pub fn allocate_eternally(&self, size: usize) -> Option<PhysicalAddress> {
-        // TODO: check if the memory does not touch the free lists.
-        let start = self.start.fetch_add(size, Ordering::SeqCst);
-
-        let usable_regions = self
-            .memory_map
-            .iter()
-            .filter(|region| region.region_type == MemoryRegionType::Usable);
-
-        let mut current = start;
-
-        for usable_region in usable_regions {
-            let region_size = usable_region.range.end_addr() - usable_region.range.start_addr();
-
-            if current + size > region_size as usize {
-                current = current.saturating_sub(region_size as usize);
-                continue;
-            }
-
-            return Some(PhysicalAddress::new(
-                usable_region.range.start_addr() as usize + current,
-            ));
-        }
-
-        None
-    }
 }
 
 pub static FRAME_ALLOC: FrameAllocator = FrameAllocator::new();
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use bootloader::bootinfo::{FrameRange, MemoryRegion};
-
-    #[test_case]
-    fn test_allocate_eternally_all_memory() {
-        let mut map = MemoryMap::new();
-        map.add_region(MemoryRegion {
-            range: FrameRange::new(0, 4096),
-            region_type: MemoryRegionType::Usable,
-        });
-
-        let alloc = FrameAllocator::new();
-        alloc.init_with(&map);
-
-        assert_eq!(
-            alloc.allocate_eternally(4095),
-            Some(PhysicalAddress::new(0))
-        );
-        assert_eq!(
-            alloc.allocate_eternally(1),
-            Some(PhysicalAddress::new(4095))
-        );
-        assert_eq!(alloc.allocate_eternally(1), None);
-    }
-
-    #[test_case]
-    fn test_allocate_eternally_in_next_chunk() {
-        let mut map = MemoryMap::new();
-        map.add_region(MemoryRegion {
-            range: FrameRange::new(0, 4096),
-            region_type: MemoryRegionType::Usable,
-        });
-
-        let second_chunk_start = 4096 * 10;
-
-        map.add_region(MemoryRegion {
-            range: FrameRange::new(second_chunk_start, second_chunk_start + 4096 * 5),
-            region_type: MemoryRegionType::Usable,
-        });
-
-        let alloc = FrameAllocator::new();
-        alloc.init_with(&map);
-        assert_eq!(
-            alloc.allocate_eternally(4097),
-            Some(PhysicalAddress::from(second_chunk_start))
-        );
-    }
-}

a838c9519ffda78d9e5b1320a936c74fee6022b0
Nathan Kolpa <nathan@kolpa.me>
2023-12-30 20:19:50 +0100

    Kernel: Implement prettier output when running unit tests

--- kernel/src/testing.rs
+++ kernel/src/testing.rs
@@ -6,31 +6,46 @@ use crate::arch::x86_64::device::qemu::{ExitCode, QEMU_DEVICE};
 use crate::{debug_print, debug_println};
 
 pub trait TestCase {
-    fn run(&self);
+    fn run(&self, test_number: usize, test_count: usize);
 }
+
+const TEST_NAME_ALIGN_TO: usize = 100;
 
 impl<T> TestCase for T
 where
     T: Fn(),
 {
-    fn run(&self) {
-        debug_print!("{}...\t", core::any::type_name::<T>());
+    fn run(&self, test_number: usize, test_count: usize) {
+        let test_name = core::any::type_name::<T>();
+        let padding = TEST_NAME_ALIGN_TO.saturating_sub(test_name.len());
+
+        debug_print!(
+            "  ({test_number}/{test_count}) =>\t{test_name}...{: <1$}",
+            "",
+            padding
+        );
         self();
         debug_println!("[ok]");
     }
 }
 
 pub fn runner(tests: &[&dyn TestCase]) {
-    debug_println!("Running {} tests", tests.len());
+    debug_println!(
+        "Running {} unit tests in post-initialization environment:",
+        tests.len()
+    );
 
-    for test in tests {
-        test.run();
+    for (i, test) in tests.iter().enumerate() {
+        test.run(i + 1, tests.len());
     }
+
+    debug_println!("All unit tests completed successfully, shutting down...");
 
     QEMU_DEVICE.lock().exit(ExitCode::Success);
 }
 pub fn panic_handler(info: &PanicInfo) -> ! {
-    debug_println!("[failed]\n");
-    debug_println!("Error: {}\n", info);
+    debug_println!("[failed]");
+    debug_println!("Error: {}", info);
+    debug_println!("Unit test failed, shutting down...");
     QEMU_DEVICE.lock().exit(ExitCode::Failed);
 }

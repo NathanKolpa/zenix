5d02536078fba3d705227fcd3b7b3963ca719fe4
Nathan Kolpa <nathan@kolpa.me>
2024-03-06 21:50:23 +0100

    Kernel: APIC WIP; Don't use the PIC and disable when apic is supported

--- kernel/src/arch/x86_64/idt.rs
+++ kernel/src/arch/x86_64/idt.rs
@@ -1,16 +1,36 @@
+use core::ops::Deref;
+
 use crate::wrap_isr;
 
 use crate::interface::interrupts as kernel_interface;
 
 use super::gdt::*;
 use essentials::spin::{Singleton, SpinLock};
-use x86_64::{device::pic_8259::ChainedPic8259, interrupt::*};
+use x86_64::{cpuid::get_features, device::pic_8259::ChainedPic8259, interrupt::*};
 
 const PIC_CHAIN_INTS_START: usize = InterruptDescriptorTable::STANDARD_INTERRUPTS_COUNT;
 const PIC_CHAIN_TICK_INT_INDEX: usize = PIC_CHAIN_INTS_START;
 
-pub static PIC_CHAIN: Singleton<SpinLock<ChainedPic8259>> =
-    Singleton::new(|| SpinLock::new(unsafe { ChainedPic8259::new(PIC_CHAIN_INTS_START as u8) }));
+enum InterruptControl {
+    Pic(SpinLock<ChainedPic8259>),
+    Apic,
+}
+
+fn init_int_control() -> InterruptControl {
+    let cpu_features = get_features();
+
+    let mut pic = unsafe { ChainedPic8259::new(PIC_CHAIN_INTS_START as u8) };
+    pic.init();
+
+    if cpu_features.apic() {
+        pic.disable();
+        return InterruptControl::Apic;
+    }
+
+    InterruptControl::Pic(SpinLock::new(pic))
+}
+
+static INTERRUPT_CONTROL: Singleton<InterruptControl> = Singleton::new(init_int_control);
 
 extern "x86-interrupt" fn double_fault_handler(frame: InterruptStackFrame, _error_code: u64) -> ! {
     panic!("Double fault: {frame:?}")
@@ -20,12 +40,18 @@ extern "x86-interrupt" fn uart_status_change_isr(_frame: InterruptStackFrame) {
     kernel_interface::uart_status_change();
 }
 
+fn apic_tick_isr(ctx_ptr: *const InterruptedContext) -> InterruptedContext {
+    let ctx = unsafe { (*ctx_ptr).clone() };
+
+    kernel_interface::tick(ctx)
+}
+
 fn tick_isr(ctx_ptr: *const InterruptedContext) -> InterruptedContext {
     let ctx = unsafe { (*ctx_ptr).clone() };
 
-    PIC_CHAIN
-        .lock()
-        .end_of_interrupt(PIC_CHAIN_TICK_INT_INDEX as u8);
+    if let InterruptControl::Pic(pic) = INTERRUPT_CONTROL.deref() {
+        without_interrupts(|| pic.lock().end_of_interrupt(PIC_CHAIN_TICK_INT_INDEX as u8));
+    }
 
     kernel_interface::tick(ctx)
 }
@@ -37,6 +63,7 @@ extern "x86-interrupt" fn unhandled_isr(_frame: InterruptStackFrame) {
 }
 
 fn init_idt() -> InterruptDescriptorTable {
+    let int_ctrl = &*INTERRUPT_CONTROL;
     let kernel_segment = GDT.kernel_code;
     let mut idt = InterruptDescriptorTable::new();
 
@@ -51,7 +78,12 @@ fn init_idt() -> InterruptDescriptorTable {
     idt[InterruptDescriptorTable::STANDARD_INTERRUPTS_COUNT + 0x04]
         .set_handler(kernel_segment, uart_status_change_isr);
 
-    idt[PIC_CHAIN_TICK_INT_INDEX].set_handler(kernel_segment, tick_isr_handler);
+    match int_ctrl {
+        InterruptControl::Pic(_) => {
+            idt[PIC_CHAIN_TICK_INT_INDEX].set_handler(kernel_segment, tick_isr_handler);
+        }
+        InterruptControl::Apic => {}
+    }
 
     idt
 }
--- kernel/src/arch/x86_64/init.rs
+++ kernel/src/arch/x86_64/init.rs
@@ -5,6 +5,4 @@ use super::idt::IDT;
 pub fn init() {
     GDT.load();
     IDT.load();
-
-    super::idt::PIC_CHAIN.lock().init();
 }

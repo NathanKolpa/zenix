0ea8223ba8b88ba88f64e41f2864ea92cad0cb67
Nathan Kolpa <nathan@kolpa.me>
2024-03-09 23:50:31 +0100

    Pre kernel: Update rename

--- pre-kernel/src/rsdp_detection.rs
+++ pre-kernel/src/rsdp_detection.rs
@@ -1,4 +1,4 @@
-use x86_64::acpi::rsdt;
+use x86_64::acpi::RSDP_SIGNATURE;
 
 pub const RSDP_ADDR_START: usize = 0x000E0000;
 pub const RSDP_ADDR_END: usize = 0x000FFFFF;
@@ -9,7 +9,7 @@ pub unsafe fn scan_special_region_for_rsdp() -> Option<u64> {
     for addr in address_range {
         let possible_signature = &*(addr as *const [u8; 8]);
 
-        if possible_signature == &rsdt::SIGNATURE {
+        if possible_signature == &RSDP_SIGNATURE {
             return Some(addr as u64);
         }
     }

a06d9f99952de8b267cf7bb1d857d93add1a441e
Nathan Kolpa <nathan@kolpa.me>
2024-03-15 12:38:08 +0100

    Kernel: remove debug_println in interrupt handler

--- kernel/src/interface/interrupts.rs
+++ kernel/src/interface/interrupts.rs
@@ -1,4 +1,4 @@
-use crate::{arch::CpuContext, debug_print, memory::map::PageFault, multitasking::SCHEDULER};
+use crate::{arch::CpuContext, memory::map::PageFault, multitasking::SCHEDULER};
 use crate::{log, warning_println};
 
 pub fn uart_status_change() {
@@ -6,7 +6,6 @@ pub fn uart_status_change() {
 }
 
 pub fn tick(current_context: CpuContext) -> CpuContext {
-    debug_print!(".");
     if let Some(next_context) = SCHEDULER.next_ctx(current_context) {
         return next_context;
     }

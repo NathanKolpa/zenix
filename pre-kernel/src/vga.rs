fd148c4323ddb22be5640edd510cb76809f0a51c
Nathan Kolpa <nathan@kolpa.me>
2024-03-15 11:10:29 +0100

    Pre kernel: use the x86_64 vga buffer

--- pre-kernel/src/vga.rs
+++ pre-kernel/src/vga.rs
@@ -1,17 +1,36 @@
-use core::mem::size_of;
-
 use essentials::address::VirtualAddress;
-use x86_64::port::Port;
+use x86_64::{
+    device::{ColouredTextBuffer, FrameBuffer, TextBufferColour, VgaBuffer},
+    port::Port,
+};
 
 pub fn set_fail_msg(message: &str) {
-    let mut cursor = msg("Failure", STATUS_START, VGA_RED, VGA_BLACK);
-    cursor = msg("Reason: ", next_line(cursor), VGA_RED, VGA_BLACK);
-    cursor = msg(message, cursor, VGA_RED, VGA_BLACK);
+    let mut cursor = msg(
+        "Failure",
+        STATUS_START,
+        TextBufferColour::Red,
+        TextBufferColour::Black,
+    );
+
+    cursor = msg(
+        "Reason: ",
+        next_line(cursor),
+        TextBufferColour::Red,
+        TextBufferColour::Black,
+    );
+
+    cursor = msg(
+        message,
+        cursor,
+        TextBufferColour::Red,
+        TextBufferColour::Black,
+    );
+
     msg(
         "Halting CPU. Reset your machine to try again.",
         next_line(cursor),
-        VGA_RED,
-        VGA_BLACK,
+        TextBufferColour::Red,
+        TextBufferColour::Black,
     );
 }
 
@@ -19,27 +38,39 @@ const RUNNING_MSG: &str = "Running pre kernel: ";
 const STATUS_START: usize = RUNNING_MSG.len();
 
 pub fn set_running_msg() {
-    msg(RUNNING_MSG, 0, VGA_WHITE, VGA_BLACK);
+    msg(
+        RUNNING_MSG,
+        0,
+        TextBufferColour::White,
+        TextBufferColour::Black,
+    );
 }
 
 pub fn set_success_msg() {
-    let cursor = msg("Ok", STATUS_START, VGA_GREEN, VGA_BLACK);
+    let cursor = msg(
+        "Ok",
+        STATUS_START,
+        TextBufferColour::Green,
+        TextBufferColour::Black,
+    );
 
     msg(
         "Entering the Zenix kernel...",
         next_line(cursor),
-        VGA_WHITE,
-        VGA_BLACK,
+        TextBufferColour::White,
+        TextBufferColour::Black,
     );
 }
 
 const fn next_line(cursor: usize) -> usize {
-    (cursor / VGA_WIDTH + 1) * VGA_WIDTH
+    (cursor / VgaBuffer::HEIGHT + 1) * VgaBuffer::WIDTH
 }
 
 pub fn clear_screen() {
-    for i in 0..VGA_SIZE {
-        write_cell(i, b' ', VGA_BLACK, VGA_BLACK);
+    let mut buffer = unsafe { VgaBuffer::new() };
+
+    for (x, y) in buffer.iter_all_pos() {
+        buffer.put_coloured(x, y, ' ', TextBufferColour::Black, TextBufferColour::Black);
     }
 
     disable_cursor();
@@ -55,41 +86,19 @@ fn disable_cursor() {
     }
 }
 
-fn msg(message: &str, start: usize, fg: u8, bg: u8) -> usize {
-    let mut cursor = start;
-    for char in message.bytes().take(VGA_SIZE) {
-        write_cell(cursor, char, fg, bg);
-        cursor = cursor.wrapping_add(1);
-    }
-    cursor
+fn msg(message: &str, start: usize, fg: TextBufferColour, bg: TextBufferColour) -> usize {
+    let mut buffer = unsafe { VgaBuffer::new() };
+
+    buffer.put_coloured_str(
+        start % buffer.width(),
+        start / buffer.width(),
+        message,
+        fg,
+        bg,
+    );
+
+    start + message.len()
 }
 
-const VGA_HEIGHT: usize = 25;
-const VGA_WIDTH: usize = 80;
-const VGA_SIZE: usize = VGA_WIDTH * VGA_HEIGHT;
-
-pub const VGA_ADDR: VirtualAddress = VirtualAddress::new(0xB8000);
-pub const VGA_LEN: usize = VGA_SIZE * size_of::<u16>();
-
-const VGA_WHITE: u8 = 15;
-const VGA_BLACK: u8 = 0;
-const VGA_RED: u8 = 12;
-const VGA_GREEN: u8 = 2;
-
-fn write_cell(pos: usize, char: u8, fg: u8, bg: u8) {
-    const SCREEN_PTR: *mut u16 = VGA_ADDR.as_mut_ptr();
-
-    if pos > VGA_SIZE {
-        return;
-    }
-
-    const COLOR_MASK: u8 = 0x0F;
-    let color = ((bg & COLOR_MASK) << 4) | (fg & COLOR_MASK);
-
-    let ascii_char = if char.is_ascii() { char } else { b'?' };
-    let cell = (ascii_char as u16) | ((color as u16) << 8);
-
-    let cell_ptr = unsafe { SCREEN_PTR.add(pos) };
-
-    unsafe { core::ptr::write_volatile(cell_ptr, cell) };
-}
+pub const VGA_ADDR: VirtualAddress = VgaBuffer::BUFFER_ADDR;
+pub const VGA_LEN: usize = VgaBuffer::BUFFER_SIZE;

c7f8d1aed1002d4d9ad4a64e227c9998dad4cea3
Nathan Kolpa <nathan@kolpa.me>
2024-03-17 16:22:33 +0100

    Essentials: Respect sign extension for virtual address

--- libraries/essentials/src/address.rs
+++ libraries/essentials/src/address.rs
@@ -1,17 +1,53 @@
-//! Abstractions around pointers.
+//! Abstractions around `void` pointers.
 
-use core::ops::{AddAssign, Sub};
+use core::ops::*;
 use core::{
     fmt::{Debug, Display, Formatter},
     marker::PhantomData,
-    ops::Add,
 };
+
+pub trait AddressMarker {
+    const SIZE: usize;
+
+    /// From phill opp's blog:
+    /// > Even though bits 48 to 64 are discarded, they can’t be set to arbitrary values.
+    /// > Instead, all bits in this range have to be copies of bit 47 in order to keep addresses unique and allow future extensions like the 5-level page table.
+    /// > This is called sign-extension because it’s very similar to the sign extension in two’s complement.
+    /// > When an address is not correctly sign-extended, the CPU throws an exception.
+    const SING_EXT: bool;
+}
 
 #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct VirtualAddressMarker {}
 
+impl AddressMarker for VirtualAddressMarker {
+    #[cfg(target_arch = "x86_64")]
+    const SIZE: usize = 48;
+
+    #[cfg(target_arch = "x86_64")]
+    const SING_EXT: bool = true;
+
+    #[cfg(target_arch = "x86")]
+    const SIZE: usize = 32;
+
+    #[cfg(target_arch = "x86")]
+    const SING_EXT: bool = false;
+}
+
 #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct PhysicalAddressMarker {}
+
+impl AddressMarker for PhysicalAddressMarker {
+    #[cfg(target_arch = "x86_64")]
+    const SIZE: usize = 52;
+    #[cfg(target_arch = "x86_64")]
+    const SING_EXT: bool = false;
+
+    #[cfg(target_arch = "x86")]
+    const SIZE: usize = 32;
+    #[cfg(target_arch = "x86")]
+    const SING_EXT: bool = false;
+}
 
 #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 #[repr(C)]
@@ -30,12 +66,29 @@ pub type PhysicalAddress = Address<PhysicalAddressMarker>;
 /// - Their internal representation differs.
 ///
 ///  See [paging introduction](https://os.phil-opp.com/paging-introduction/#paging-on-x86-64) for a more detailed explanation.
-impl<L> Address<L> {
+impl<L> Address<L>
+where
+    L: AddressMarker,
+{
+    const ADDR_MASK: usize = usize::MAX >> (usize::BITS as usize - L::SIZE);
+    const VALUE_MASK: usize = !Self::ADDR_MASK;
+
     pub const fn new(addr: usize) -> Self {
         Self {
             addr,
             _phantom: PhantomData,
         }
+    }
+
+    fn new_modified(mut addr: usize) -> Self {
+        addr &= Self::ADDR_MASK;
+
+        if L::SING_EXT {
+            let multiplier = Self::ADDR_MASK & addr >> L::SIZE.saturating_sub(1);
+            addr |= multiplier * Self::VALUE_MASK;
+        }
+
+        Self::new(addr)
     }
 
     pub const fn null() -> Self {
@@ -47,7 +100,7 @@ impl<L> Address<L> {
 
     pub const fn max() -> Self {
         Self {
-            addr: usize::MAX,
+            addr: Self::ADDR_MASK,
             _phantom: PhantomData,
         }
     }
@@ -93,30 +146,39 @@ impl<L> Address<L> {
     pub fn is_null(&self) -> bool {
         self.addr == 0
     }
+
+    pub fn align_down(&self, align: usize) -> Self {
+        Self::new_modified(Self::align_ptr_down(self.addr, align))
+    }
+
+    pub fn align_up(&self, align: usize) -> Self {
+        Self::new_modified(Self::align_ptr_up(self.addr, align))
+    }
+
+    pub fn checked_add(&self, rhs: usize) -> Option<Self> {
+        let value = self.addr.checked_add(rhs)?;
+
+        if ((rhs & Self::ADDR_MASK) + (self.addr & Self::ADDR_MASK)) & Self::ADDR_MASK
+            > usize::MAX & Self::ADDR_MASK
+        {
+            return None;
+        }
+
+        Some(Self::new_modified(value))
+    }
 }
 
-impl<L> Default for Address<L> {
+impl<L: AddressMarker> Default for Address<L> {
     fn default() -> Self {
         Self::null()
     }
 }
 
 /// A wrapper for physical addresses.
-impl Address<PhysicalAddressMarker> {
-    pub fn align_down(&self, align: usize) -> Self {
-        Self::new(Self::align_ptr_down(self.addr, align))
-    }
-}
+impl Address<PhysicalAddressMarker> {}
 
 /// A wrapper for virtual addresses, or normal pointers.
 impl Address<VirtualAddressMarker> {
-    pub const fn align_down(&self, align: usize) -> Self {
-        let mut addr = self.addr;
-        addr = Self::align_ptr_down(addr, align);
-        addr = ((addr << 16) as isize >> 16) as usize;
-        Self::new(addr)
-    }
-
     #[cfg(target_arch = "x86_64")]
     #[doc(cfg(target_arch = "x86_64"))]
     fn truncate_index(value: usize) -> usize {
@@ -192,63 +254,63 @@ impl Address<VirtualAddressMarker> {
     }
 }
 
-impl<T> Add<usize> for Address<T> {
+impl<T: AddressMarker> Add<usize> for Address<T> {
     type Output = Self;
 
     fn add(self, rhs: usize) -> Self::Output {
-        Self::new(self.addr + rhs)
+        Self::new_modified(self.addr + rhs)
     }
 }
 
-impl<T> AddAssign<usize> for Address<T> {
+impl<T: AddressMarker> AddAssign<usize> for Address<T> {
     fn add_assign(&mut self, rhs: usize) {
         self.addr += rhs;
     }
 }
 
-impl<T> Add<Address<T>> for Address<T> {
+impl<T: AddressMarker> Add<Address<T>> for Address<T> {
     type Output = Self;
 
     fn add(self, rhs: Address<T>) -> Self::Output {
-        Self::new(self.addr + rhs.addr)
+        Self::new_modified(self.addr + rhs.addr)
     }
 }
 
-impl<T> Sub<usize> for Address<T> {
+impl<T: AddressMarker> Sub<usize> for Address<T> {
     type Output = Self;
 
     fn sub(self, rhs: usize) -> Self::Output {
-        Self::new(self.addr - rhs)
+        Self::new_modified(self.addr - rhs)
     }
 }
 
-impl<T> Sub<Address<T>> for Address<T> {
+impl<T: AddressMarker> Sub<Address<T>> for Address<T> {
     type Output = Self;
 
     fn sub(self, rhs: Address<T>) -> Self::Output {
-        Self::new(self.addr - rhs.addr)
+        Self::new_modified(self.addr - rhs.addr)
     }
 }
 
-impl<L> From<usize> for Address<L> {
+impl<L: AddressMarker> From<usize> for Address<L> {
     fn from(value: usize) -> Self {
         Self::new(value)
     }
 }
 
-impl<L> From<u64> for Address<L> {
+impl<L: AddressMarker> From<u64> for Address<L> {
     fn from(value: u64) -> Self {
         Self::new(value as usize)
     }
 }
 
-impl<L, T> From<*const T> for Address<L> {
+impl<L: AddressMarker, T> From<*const T> for Address<L> {
     fn from(value: *const T) -> Self {
         Self::new(value as usize)
     }
 }
 
-impl<L, T> From<*mut T> for Address<L> {
+impl<L: AddressMarker, T> From<*mut T> for Address<L> {
     fn from(value: *mut T) -> Self {
         Self::new(value as usize)
     }
@@ -322,7 +384,19 @@ mod tests {
         let source_indices = addr.indices();
         let new_addr = VirtualAddress::from_indices(source_indices);
         let new_indices = new_addr.indices();
-
         assert_eq!(source_indices, new_indices);
+    }
+
+    #[test_case]
+    #[cfg(target_arch = "x86_64")]
+    fn test_sing_ext() {
+        let last_bit_set = 1usize << 47;
+        let addr = VirtualAddress::new(last_bit_set);
+
+        assert_eq!(
+            (addr.as_u64() & VirtualAddress::VALUE_MASK as u64)
+                >> VirtualAddressMarker::SIZE as u64,
+            VirtualAddress::VALUE_MASK as u64 >> VirtualAddressMarker::SIZE as u64
+        )
     }
 }
